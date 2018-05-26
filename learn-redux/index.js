f84b49817323c58836e81b6c467e8f68db7afaf7
huangjian <mr.huangjian@foxmail.com>
2018-05-27 00:58:33 +0800

    m

---
+++ learn-redux/index.js
@@ -0,0 +1,43 @@
+/**
+ * create by:   huangjian
+ * create date: 2018-05-26 21:30
+ * description: ##
+ */
+
+import * as Redux from "redux";
+
+const ActionTypes = {
+  ALL: Symbol("ALL")
+};
+
+const defaultState = {
+  users: []
+}
+
+// 制定规则
+function getUsers(state, action) {
+  state = state || defaultState;
+
+  switch (action.type) {
+    case ActionTypes.ALL:
+      return Object.assign({}, state, {
+        users: [1, 2, 3]
+      });
+    default:
+      return state;
+  }
+}
+
+// 创建容器 (调用rule，即先初始化store)
+const store = Redux.createStore(getUsers);
+
+// 添加监听 (store中的state发生改变后，监听接收)
+store.subscribe(function(){
+  let currentState = store.getState();
+  currentState.users.push(4, 5, 6);
+});
+
+// 触发事件
+store.dispatch({
+  type: ActionTypes.ALL
+})
