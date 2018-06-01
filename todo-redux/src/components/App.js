9a959c4a899c26b04cac240551372dc17f13493f
huangjian <mr.huangjian@foxmail.com>
2018-06-01 21:33:21 +0800

    m

--- todo-redux/src/components/App.js
+++ todo-redux/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import AddTodo from '../containers/AddTodo';
 import VisibleTodoList from '../containers/VisibleTodoList';
 import Footer from './Footer';
