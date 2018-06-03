b99313e02e347eaa5e369fb41ae33c45d9c0dd46
huangjian <mr.huangjian@foxmail.com>
2018-06-04 00:46:41 +0800

    提交

--- test-redux/src/index.js
+++ test-redux/src/index.js
@@ -1,8 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore,bindActionCreators} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function reducer(state = {name: 'guest', number: 0}, action) {
+  switch (action.type) {
+    case 'changeName':
+      return Object.assign({}, state, {name: action.name})
+    case 'access':
+      return Object.assign({}, state, {number: ++state.number})
+    default:
+      return state
+  }
+}
+
+const actionMethods = {
+  changeName(name){
+    return {
+      type: 'changeName',
+      name
+    }
+  },
+  access(){
+    return {
+      type: 'access'
+    }
+  }
+}
+
+const store = createStore(reducer)
+const actions = bindActionCreators(actionMethods, store.dispatch)
+
+function render() {
+  let state = store.getState()
+
+  ReactDOM.render(<App
+    name={state.name}
+    number={state.number}
+    access={actions.access}
+    changeName={actions.changeName}
+    />, document.getElementById('root'))
+}
+
+store.subscribe(render)
+render()
+
 registerServiceWorker();
